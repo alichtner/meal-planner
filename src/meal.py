c3ac900c9466aaa16f2665251340d05d28a9887d
Aaron Lichtner <aaron.lichtner@nordstrom.com>
2019-12-17 16:15:44 -0800

    some ingredients

--- src/meal.py
+++ src/meal.py
@@ -4,4 +4,12 @@ class Meal(object):
     def __init__(self):
         self.name
         self.meal_number
+        self.meal_type
         self.macros
+        self.calories
+        self.is_leftover
+        self.can_be_leftover
+        self.ingredients
+
+    def generate(self):
+        pass
